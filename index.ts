d862e1e2482fb2575c50ef1a75ccdf6abd2b8ad8
fernandobom92 <fernandobom92@gmail.com>
2022-07-30 16:43:38 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const url = 'http://jsonplaceholder.typicode.com/todos/1';
+
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;    //true or false
+}
+
+axios.get(url).then(response => {
+  const todo = response.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  logTodo(id, title, completed);
+});
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`The todo has an ID: ${id}, has a title of ${title}. Is it finished? ${completed}`);
+};
